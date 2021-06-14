4604776e60dc95f6403928dc9fac5e13f696ba0b
Dylan Rae <55325297+djohnjames@users.noreply.github.com>
2021-06-14 13:58:07 -0600

    Moved header and footers to a seperate file so they do not need to be changed on the html for every page. jQuery is used and function was added to main javascript file

--- assets/js/main.js
+++ assets/js/main.js
@@ -220,4 +220,10 @@
     });
   }
 
+  // Function for loading header and footer
+  $(function(){
+    $("#nav-placeholder").load("nav.html");
+    $("#footer-placeholder").load("footer.html");
+  });
+
 })()
