e97443c2da0bd26b5da3538c2631be14b47a987a
Dylan Rae <55325297+djohnjames@users.noreply.github.com>
2021-07-31 19:23:31 -0600

    Removed dependence on CSI library

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,25 +110,25 @@
 
   
 
-  // /**
-  //  * Mobile nav toggle
-  //  */
-  // on('click', '.mobile-nav-toggle', function(e) {
-  //   select('#navbar').classList.toggle('navbar-mobile')
-  //   // select('#nav-placeholder').classList.toggle('navbar-mobile')
-  //   this.classList.toggle('bi-list')
-  //   this.classList.toggle('bi-x')
-  // })
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    // select('#nav-placeholder').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
 
-  // /**
-  //  * Mobile nav dropdowns activate
-  //  */
-  // on('click', '.navbar .dropdown > a', function(e) {
-  //   if (select('#navbar').classList.contains('navbar-mobile')) {
-  //     e.preventDefault()
-  //     this.nextElementSibling.classList.toggle('dropdown-active')
-  //   }
-  // }, true)
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
 
 
   /**
